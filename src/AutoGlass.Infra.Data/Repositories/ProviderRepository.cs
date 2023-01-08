354ad095776ef254f63af202e976abe6524e7de2
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-01-08 01:25:49 -0300

    ProviderRepository added.

---
+++ src/AutoGlass.Infra.Data/Repositories/ProviderRepository.cs
@@ -0,0 +1,11 @@
+﻿using AutoGlass.Domain.Entities;
+using AutoGlass.Infra.Data.Context;
+using AutoGlass.Infra.Data.Repositories.Core;
+
+namespace AutoGlass.Infra.Data.Repositories;
+
+public sealed class ProviderRepository : Repository<Provider>
+{
+    public ProviderRepository(AutoGlassContext context) : base(context)
+    { }
+}
